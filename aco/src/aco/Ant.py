a26e132ec61fa7236f9e99d3aa926e38e0e8d3fe
esemsc-bg721 <bg721@ic.ac.uk>
2025-05-29 17:47:06 +0100

    Started building Ant and Map classes.

--- aco/src/aco/Ant.py
+++ aco/src/aco/Ant.py
@@ -1,3 +1,46 @@
-class Ant():
-    def test(self):
-        print("Hello world from ants!")
+class Ant:
+    def __init__(self, number_of_nodes: int):
+        # Parameter to control impact of pheromones on path decision making
+        self.alpha = 1
+
+        # Parameter to control impact of desirability on path decision making
+        # In the case of the TSP, desirability is distance (shorter is better)
+        self.beta = 6
+
+        # Param to control how much pheromone ants leave when retracing path
+        self.Q = 1
+
+        # Path the ant has taken so far
+        # If this were [0, 3], it started at node 0 and went to node 3
+        self.path = []
+
+    def move_probabilities(self, map_instance):
+        """
+        Returns the probabilities of an ant going from its current state
+        to each other node it has not yet visited
+        """
+        pass
+
+    def choose_node(self, map_instance):
+        """
+        Choose next node given the possible remaining nodes and probabilities
+        """
+        pass
+
+    def calculate_tour_length(self, map_instance):
+        """
+        Compute length of total tour. Should only be called at end of tour.
+        """
+        pass
+
+    def deposit_pheremones(self, map_instance):
+        """"
+        Deposit pheremones on the path visited
+        """
+        pass
+
+    def move(self, map_instance):
+        """
+        Move the ant from its current node to the next
+        """
+        pass
---
+++ aco/src/aco/Map.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+class Map:
+    """
+    Class to keep track of the map, keeping track of nodes, pheromones and ants
+    """
+
+    def __init__(self, nodes: list, ants: list):
+        """
+        nodes: list of tuples holding coordinates for the location of nodes
+        """
+        self.nodes = nodes
+
+        # Matrix to store the distance between nodes
+        # Eg: if distance_matrx[1, 2] = 12, the distance between node 1 and node 2 is 12
+        # This matrix is currently symmetric, but doesn't need to be
+        self.distance_matrix = np.zeros((len(nodes, len(nodes))))
+        self._compute_distance_matrix()
+
+        # Matrix to store pheromone levels between nodes
+        # The indexing is the same as above, for example:
+        # pheromone_matrix[1, 2] = 0.7 means the amount of pheromone deposited between node 1 and 2 is 0.7
+        self.pheromone_matrix = np.full((len(nodes), len(nodes)), 0.01)
+
+        # Controls how quickly pheromone trails evaporate
+        self.pheromone_evaporation_coefficient = 0.6
+
+        # Matrix to store desirability of transition from node x to node y
+        # desirability_matrix[1, 2] = 0.4 means the desirability to go from node 1 to node 2 is 0.4
+        self.desirability_matrix = 1 / self.distance_matrix
+
+        # List of ant objects
+        self.ants = ants
+
+    def calculate_distance(self, beginning, end):
+        """
+        Compute the euclidean distance between two nodes
+        """
+        beg_vec = np.array(beginning)
+        end_vec = np.array(end)
+
+        return np.linalg.norm(beg_vec - end_vec)
+
+    def _compute_distance_matrix(self):
+        """
+        Generate the distance matrix using the euclidean norm
+        """
+        for node1_idx in range(len(self.nodes)):
+            for node2_idx in range(len(self.nodes)):
+                distance = self.calculate_distance(
+                    self.nodes[node1_idx], self.nodes[node2_idx])
+                if distance == 0:
+                    distance = 0.01
+                self.distance_matrix[node1_idx, node2_idx] = distance
+
+    def pheromone_decay(self):
+        """
+        Decays existing pheromone trails
+        To be performed before the ants update trails
+        """
+        self.pheromone_matrix *= self.pheromone_evaporation_coefficient
+
+    def move_ants(self):
+        """
+        Move all ants one step forward
+        """
+        for ant in self.ants:
+            ant.move(self)
