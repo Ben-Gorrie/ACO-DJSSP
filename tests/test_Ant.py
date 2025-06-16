6d9a8464de0d0726d88a6d2ea27e7b52b25e3bac
esemsc-bg721 <bg721@ic.ac.uk>
2025-06-16 14:51:05 +0100

    Changed tests for Ant for JSSP

--- tests/test_Ant.py
+++ tests/test_Ant.py
@@ -1,115 +1,110 @@
-from aco import Map, Ant
-import numpy as np
+from aco import Map, Ant, generate_operations_from_jobs
 import math
+import numpy as np
 
-nodes = [
-    (54, 67),
-    (54, 62),
-    (37, 84),
-    (41, 94),
-    (2, 99),
-    (7, 64),
-    (25, 62),
-    (22, 60),
-    (18, 54),
-    (4, 50),
-    (13, 40),
-    (18, 40),
-    (24, 42),
-    (25, 38),
-    (44, 35),
-    (41, 26),
-    (45, 21),
-    (58, 35),
-    (62, 32),
-    (82,  7),
-    (91, 38),
-    (83, 46),
-    (71, 44),
-    (64, 60),
-    (68, 58),
-    (83, 69),
-    (87, 76),
-    (74, 78),
-    (71, 71),
-    (58, 69)
+jobs = [
+    [(0, 5), (1, 3)],  # Job 0
+    [(1, 4), (0, 2)]  # Job 1
 ]
 
-ants = [Ant() for i in range(len(nodes))]
+# Convert to list of Operation objects
+operations = generate_operations_from_jobs(jobs)
 
-map_instance = Map(nodes, ants)
+ants = [Ant() for _ in range(10)]
+map_instance = Map(operations, ants)
 
-ant = Ant()
-path = [1, 5, 4]
-ant.path = path
+
+def test_get_eligible_operations():
+    my_ant = Ant()
+
+    eligible = my_ant.get_eligible_operations(operations)
+
+    # Assert that the only eligible operations
+    # at this initial stage are the ones that start a job
+    for op in eligible:
+        assert op.operation_id == 0
+
+    # Assert that there are only as many eligible
+    # operations as there are jobs
+    assert len(eligible) == len(jobs)
+
+    # Manually add the first operation to the schedule of the ant
+    my_ant.path.append(operations[0])
+
+    eligible_updated = my_ant.get_eligible_operations(operations)
+
+    # Assert that the new eligible operations are the one
+    # following operation (0, 5) or the one starting a new job
+    for op in eligible_updated:
+        assert (op.operation_id == 0 or op.processing_time == 3)
 
 
 def test_move_probabilities():
-    # Make sure that the remaining nodes are correct
-    probabilities, nodes_remaining = ant.move_probabilities(map_instance)
-    assert (nodes_remaining == np.delete(np.array([i for i in range(
-        len(nodes))]), path)).all()
-    assert len(nodes_remaining) == len(nodes) - len(path)
+
+    my_ant = Ant()
+    eligible = my_ant.get_eligible_operations(operations)
+    probabilities = my_ant.move_probabilities(map_instance, eligible)
 
     # Ensure probabilites add to 1
     assert math.isclose(np.sum(probabilities), 1)
 
+    # Manually add the first operation to the schedule of the ant
+    my_ant.path.append(operations[0])
 
-def test_choose_node():
-    # Increase probability the ant goes to node 10
-    map_instance.pheromone_matrix[4, 10] *= 10
+    eligible_updated = my_ant.get_eligible_operations(operations)
+    probabilities_updated = my_ant.move_probabilities(
+        map_instance, eligible_updated)
+
+    # Ensure probabilites add to 1
+    assert math.isclose(np.sum(probabilities_updated), 1)
+
+
+def test_choose_operation():
+    my_ant = Ant()
+
+    eligible = my_ant.get_eligible_operations(operations)
+    chosen_op = my_ant.choose_operation(map_instance, eligible)
+
+    # Ensure chosen operation is not already in the ant's path
+    assert chosen_op not in my_ant.path
+    # Ensure that chosen_op is in the list of all operations
+    # and the list of eligible operations
+    assert chosen_op in operations
+    assert chosen_op in eligible
+
+    # Manually add the first operation to the schedule of the ant
+    my_ant.path.append(operations[0])
+    eligible = my_ant.get_eligible_operations(operations)
+
+    # Increase probability the ant goes to operation 1 from operation 0
+    map_instance.pheromone_matrix[0, 1] *= 2
 
     counter = 0
 
     for i in range(100):
-        choice = ant.choose_node(map_instance)
+        chosen_op = my_ant.choose_operation(map_instance, eligible)
 
-        # Ensure the chosen node is not where the ant has already been
-        assert choice not in path
-        assert choice < len(nodes)
+        # Ensure chosen operation is not already in the ant's path
+        assert chosen_op not in my_ant.path
+        # Ensure that chosen_op is in the list of all operations
+        # and the list of eligible operations
+        assert chosen_op in operations
+        assert chosen_op in eligible
 
-        if choice == 10:
+        if chosen_op.index == 1:
             counter += 1
 
-    # Ensure the ant is weighted towards going to the 10th node
-    assert counter > 5
+    # Ensure the ant is weighted towards going to operation 1
+    assert counter > 80
 
 
-def test_calculate_tour_length():
-    map_instance.distance_matrix[1, 5] = 3.4
-    map_instance.distance_matrix[5, 4] = 2.7
+def test_construct_schedule():
+    my_ant = Ant()
 
-    # Ensure tour length is calculated correctly
-    assert ant.calculate_tour_length(map_instance) == 3.4 + 2.7
+    my_ant.construct_schedule(map_instance)
 
+    # Ensure all operations are added to the schedule
+    assert len(my_ant.path) == len(operations)
 
-def test_deposit_pheromones():
-    old_pheromones = map_instance.pheromone_matrix.copy()
-    ant.deposit_pheromones(map_instance)
-    new_pheromones = map_instance.pheromone_matrix.copy()
-
-    # Ensure that adding new pheromones increases the pheromone trail
-    assert old_pheromones[1, 5] < new_pheromones[1, 5]
-    assert old_pheromones[5, 4] < new_pheromones[5, 4]
-
-
-def test_move():
-    old_path = ant.path.copy()
-    ant.move(map_instance)
-    new_path = ant.path.copy()
-
-    # Make sure we are not closing out the path
-    if len(old_path) < len(nodes):
-        # Ensure that the new path is longer than the old path
-        assert len(old_path) < len(new_path)
-
-        # Ensure that the newly added node was not already in the path
-        assert new_path[-1] not in old_path
-
-
-def test_reset_position():
-    old_path = ant.path.copy()
-    ant.reset_position()
-
-    # Ensure that resetting the position of the ant is done correctly
-    assert old_path[0] == ant.path[0]
+    # Ensure starting operation starts a job
+    assert my_ant.path[0].operation_id == 0
